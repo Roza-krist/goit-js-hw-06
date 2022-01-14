4a6e1fdf84a45bd8cc354c581d43b9450dfc37cc
Roza-krist <rozakristjuhan@gmail.com>
2022-01-14 16:06:33 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", formLog);
 
+function formLog(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields");
+  }
+  console.log(`email: ${email.value}, password: ${password.value}`);
+  event.currentTarget.reset();
+}
