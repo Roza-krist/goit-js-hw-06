cb14b109fe4558636e0473a550ba1c8531cd3727
Roza-krist <rozakristjuhan@gmail.com>
2022-01-17 18:38:16 +0200

    update task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const fontSizeControl = document.querySelector("#font-size-control");
 const spanText = document.querySelector("#text");
-spanText.style.fontSize = fontSizeControl + "px";
+spanText.style.fontSize = fontSizeControl.value + "px";
 fontSizeControl.addEventListener("input", (event) => {
   spanText.style.fontSize = `${event.currentTarget.value}px`;
   console.log(`${event.currentTarget.value}px`);
