203a906a8eb96724f8d9096697a677b536596247
Roza-krist <rozakristjuhan@gmail.com>
2022-01-17 09:41:24 +0200

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -4,20 +4,7 @@ console.log(`Number of categories: `, categoriesLength.length);
 const categoryAndElements = document.querySelectorAll(".item h2");
 // console.log(categoryAndElements);
 
-let titleEl = categoryAndElements[0];
-console.log(`Category: ${titleEl.textContent}`);
-let animalList = titleEl.nextElementSibling;
-let animalListLength = animalList.childElementCount;
-console.log(`Elements: ${animalListLength}`);
-
-titleEl = categoryAndElements[1];
-console.log(`Category: ${titleEl.textContent}`);
-animalList = titleEl.nextElementSibling;
-animalListLength = animalList.childElementCount;
-console.log(`Elements: ${animalListLength}`);
-
-titleEl = categoryAndElements[2];
-console.log(`Category: ${titleEl.textContent}`);
-animalList = titleEl.nextElementSibling;
-animalListLength = animalList.childElementCount;
-console.log(`Elements: ${animalListLength}`);
+categoryAndElements.forEach((element) => {
+  console.log("Category:", element.textContent);
+  console.log("Elements:", element.nextElementSibling.children.length);
+});
